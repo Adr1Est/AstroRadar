dafa5c00fad908394cb51e45a22a9605794efd8f
Adrián Estévez <100156445+Adr1Est@users.noreply.github.com>
2025-07-24 10:22:43 +0200

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -3,12 +3,4 @@ set -o errexit
 
 cd "$(dirname "$0")"
 
-pipenv install --deploy
-
-export FLASK_APP=src/app.py
-
-pipenv run flask db upgrade --directory src/migrations
-
-cd src/front/
-npm install
-npm run build
+pipenv install 
